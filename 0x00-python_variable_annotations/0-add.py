eda378878833d2d57e94f8ef9a7b786cb65c6fdf
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-07-05 03:27:48 +0100

    fix pycodestyle error

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,8 +4,9 @@ Write a type-annotated function add that takes a float as arguments
 and returns a float
 """
 
+
 def add(a: float, b: float) -> float:
     """
     returns tge sum of a and b
     """
-    return a + b;
+    return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,8 +4,9 @@ Write a type-annotated function concat that takes a string as arguments
 and returns a string
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """
     returns the concatenation of a and b
     """
-    return str1 + str2;
+    return str1 + str2
