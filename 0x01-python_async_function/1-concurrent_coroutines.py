c0d88b3ef2f9d4a8daa4c335cf274e48e68fb135
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-07-09 02:58:53 +0100

    return a task object

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,7 +3,6 @@
 Execute multiple coroutines at the same time with async
 """
 import asyncio
-import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+task_wait_random takes an integer max_delay and returns a asyncio.Task
+"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    return asyncio.create_task(wait_random(max_delay))
