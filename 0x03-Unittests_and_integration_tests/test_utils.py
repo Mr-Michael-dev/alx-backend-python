ff2b3ba815eb2a03ba80865830a23a1fd9044a37
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-07-29 21:23:19 +0100

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -44,6 +44,7 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
+
 class TestGetJson(unittest.TestCase):
     """Test class to test get_json"""
     @parameterized.expand([
@@ -59,3 +60,25 @@ class TestGetJson(unittest.TestCase):
         mock_get.return_value.json.return_value = expected
         self.assertEqual(get_json(test_url), expected)
         mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test class to test memoize"""
+    def test_memoize(self) -> None:
+        """Test memoize"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        @patch('TestClass.a_method')
+        def test_a_method(mock_a_method: Callable) -> None:
+            """Test a_method"""
+            test_object = TestClass()
+            mock_a_method.return_value = 43
+            self.assertEqual(test_object.a_property, 43)
+            self.assertEqual(test_object.a_property, 43)
+            mock_a_method.assert_called_once()
